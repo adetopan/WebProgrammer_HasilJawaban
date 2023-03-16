8c1f41c42ce8fe2abf764f94c27557b162c4e762
adetopan <yogayoga553@gmail.com>
2023-03-16 22:32:48 +0700

    update jawaban

--- index.php
+++ index.php
@@ -34,9 +34,8 @@
   header('Content-Type: application/json');
   echo json_encode(getRoomStats($array1));
 
-  // 1.b
-  function getDoctorStats($data)
-    {
+    // 1.b
+    function getDoctorStats($data){
         $doctorStats = [];
         foreach ($data as $entry) {
             $doctorName = $entry['namadokter'];
@@ -66,61 +65,62 @@
     header('Content-Type: application/json');
     echo json_encode(getDoctorStats($array1));
 
-  
-
-  // 2
-  function countLetterInName($data, $letter)
-  {
+    // 2
+    function countLetterInName($data, $letter){
       $count = 0;
       foreach ($data as $entry) {
           $name = strtoupper($entry['namapasien']); // Ubah nama pasien menjadi huruf kapital
           $count += substr_count($name, strtoupper($letter));
       }
       return $count;
-  }
+    }
 
-  // Menerima data JSON melalui metode POST
-  $jsonData = file_get_contents('php://input');
-  $array1 = json_decode($jsonData, true);
+    // Menerima data JSON melalui metode POST
+    $jsonData = file_get_contents('php://input');
+    $array1 = json_decode($jsonData, true);
 
-  // Jika data tidak valid, kembalikan pesan kesalahan
-  if (!is_array($array1)) {
-      http_response_code(400);
-      echo json_encode(['error' => 'Invalid input data. Please provide a valid JSON array.']);
-      exit;
-  }
+    // Jika data tidak valid, kembalikan pesan kesalahan
+    if (!is_array($array1)) {
+        http_response_code(400);
+        echo json_encode(['error' => 'Invalid input data. Please provide a valid JSON array.']);
+        exit;
+    }
 
-  // Menerima huruf yang ingin dihitung melalui query parameter
-  $letter = isset($_GET['letter']) ? $_GET['letter'] : '';
+    // Menerima huruf yang ingin dihitung melalui query parameter
+    $letter = isset($_GET['letter']) ? $_GET['letter'] : '';
 
-  // Jika huruf tidak valid, kembalikan pesan kesalahan
-  if (strlen($letter) != 1 || !ctype_alpha($letter)) {
-      http_response_code(400);
-      echo json_encode(['error' => 'Invalid letter. Please provide a single alphabetical character.']);
-      exit;`
-  }
+    // Jika huruf tidak valid, kembalikan pesan kesalahan
+    if (strlen($letter) != 1 || !ctype_alpha($letter)) {
+        http_response_code(400);
+        echo json_encode(['error' => 'Invalid letter. Please provide a single alphabetical character.']);
+        exit;
+    }
 
-  header('Content-Type: application/json');
-  echo json_encode([
-      'letter' => strtoupper($letter),
-      'count' => countLetterInName($array1, $letter),
-  ]);
+    header('Content-Type: application/json');
+    echo json_encode([
+        'letter' => strtoupper($letter),
+        'count' => countLetterInName($array1, $letter),
+    ]);
 
 
+    // 3
+    function bebas($data){
+        if
+    }
 
-  // 3
-  function getPatientData($data) {
-      $response = array_map(function ($item) {
-          return [
-              'namapasien' => $item['namapasien'],
-              'tglregistrasi' => $item['tglregistrasi']
-          ];
-      }, $data);
 
-      return $response;
-  }
+    function getPatientData($data) {
+        $response = array_map(function ($item) {
+            return [
+                'namapasien' => $item['namapasien'],
+                'tglregistrasi' => $item['tglregistrasi']
+            ];
+        }, $data);
 
-  // Menerima data JSON melalui metode POST
+        return $response;
+    }
+
+    // Menerima data JSON melalui metode POST
     $jsonData = file_get_contents('php://input');
     $array1 = json_decode($jsonData, true);
 
@@ -133,34 +133,33 @@
 
     header('Content-Type: application/json');
     echo json_encode(getPatientData($array1));
+    
 
-
-
-// 4
-function addNumbering($data)
-{
-    $numberedData = [];
-    $i = 1;
-    foreach ($data as $entry) {
-        $entry['nomor'] = $i++;
-        $numberedData[] = $entry;
+    // 4
+    function addNumbering($data)
+    {
+        $numberedData = [];
+        $i = 1;
+        foreach ($data as $entry) {
+            $entry['nomor'] = $i++;
+            $numberedData[] = $entry;
+        }
+        return $numberedData;
     }
-    return $numberedData;
-}
 
-// Menerima data JSON melalui metode POST
-$jsonData = file_get_contents('php://input');
-$array1 = json_decode($jsonData, true);
+    // Menerima data JSON melalui metode POST
+    $jsonData = file_get_contents('php://input');
+    $array1 = json_decode($jsonData, true);
 
-// Jika data tidak valid, kembalikan pesan kesalahan
-if (!is_array($array1)) {
-    http_response_code(400);
-    echo json_encode(['error' => 'Invalid input data. Please provide a valid JSON array.']);
-    exit;
-}
+    // Jika data tidak valid, kembalikan pesan kesalahan
+    if (!is_array($array1)) {
+        http_response_code(400);
+        echo json_encode(['error' => 'Invalid input data. Please provide a valid JSON array.']);
+        exit;
+    }
 
-header('Content-Type: application/json');
-echo json_encode(addNumbering($array1));
+    header('Content-Type: application/json');
+    echo json_encode(addNumbering($array1));
 
 ?>
 
